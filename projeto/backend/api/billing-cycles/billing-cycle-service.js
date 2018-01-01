dbfb8c75f1ff472aab983b561494a1d24a8c48f1
Rodrigo Albornoz <rodrigo.albornoz.f@gmail.com>
2018-01-01 18:05:38 -0200

    added project

---
+++ projeto/backend/api/billing-cycles/billing-cycle-service.js
@@ -0,0 +1,5 @@
+const BillingCycle = require('./billing-cycle')
+
+BillingCycle.methods(['get','post','put','delete'])
+
+module.exports = BillingCycle
---
+++ projeto/backend/config/routes.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+module.exports = function(server) {
+	const router = express.Router()
+	server.use('/api', router)
+
+	router.route('/teste').get(function(req, res, next) {
+		res.send('funcionou');
+	})
+}
